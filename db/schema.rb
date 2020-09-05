5df4f1df82ba3dcf60dcbb0ec45fd6db1c4fe7ee
Selamawit Abdo <selamawit.t.abdo@gmail.com>
2020-09-05 17:56:30 -0600

    person added

---
+++ db/schema.rb
@@ -0,0 +1,28 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_09_05_233642) do
+
+  create_table "people", force: :cascade do |t|
+    t.string "person_id"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "address_line_one"
+    t.string "city"
+    t.string "state"
+    t.string "zip_code"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
